112d0817d3d0faf9a0437e15be55ce514fe61387
EnhancedTarget <levente.kalman922@gmail.com>
2023-09-22 12:09:14 +0100

    exercise 1 practise

---
+++ Week1Lab/Exercise1.js
@@ -0,0 +1,27 @@
+console.log("Hello World");
+
+
+//all of this does the same things in slightly different ways.
+function SayHello(){
+    console.log("Hello");
+}
+
+SayHello();
+
+const myFunction = function sayHi(){
+    console.log("Hi");
+}
+
+myFunction();
+
+const myNewFunction = function(){
+    console.log("Oi");
+}
+
+myNewFunction();
+
+const myComponent = ()=>{
+    console.log("Bye");
+}
+
+myComponent();
