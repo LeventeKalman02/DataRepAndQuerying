e2480dd2b1c6f556df7bd17dee847d54d0a438b8
EnhancedTarget <levente.kalman922@gmail.com>
2023-10-06 12:15:55 +0100

    added navbar

--- Week3Lab/book/src/App.js
+++ Week3Lab/book/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -10,6 +10,17 @@ import Footer from './Components/Footer';
 function App() {
   return (
     <div className="App">
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      
       <Header></Header>
       <Content></Content>
       <Footer></Footer>
