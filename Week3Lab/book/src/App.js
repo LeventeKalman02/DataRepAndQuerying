f82940cd951437d2a7b5d6671d8d8bb9f0779df2
EnhancedTarget <levente.kalman922@gmail.com>
2023-10-06 12:43:27 +0100

    routing working

--- Week3Lab/book/src/App.js
+++ Week3Lab/book/src/App.js
@@ -12,16 +12,18 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
+
         <Navbar bg="primary" data-bs-theme="dark">
           <Container>
             <Navbar.Brand href="#home">Navbar</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="home">Home</Nav.Link>
+              <Nav.Link href="features">Features</Nav.Link>
+              <Nav.Link href="pricing">Pricing</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
+
         <Routes>
           <Route path='/home' element={<Header></Header>}></Route>
           <Route path='/features' element={<Content></Content>}></Route>
