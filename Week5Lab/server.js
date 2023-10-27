1bfc6c00d7298eb32c7de0bf82ce436ed10bae4a
EnhancedTarget <levente.kalman922@gmail.com>
2023-10-27 11:42:22 +0100

    w5: part 3 and 4 done

--- Week5Lab/server.js
+++ Week5Lab/server.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 4000;
 
+//listening at local host 4000 for http request
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
+//the url is localhost:4000/datarep
+app.get('/datarep', (req, res) => {
+    res.send('Welcome to dataRep and Querying!');
+});
+
+//the url is localhost:4000/hello
+app.get('/hello', (req, res) =>{
+    res.send("Hello");
+});
+
+//listen for requests coming in
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
