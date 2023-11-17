7197e8f91b946e0578e978b7540a97861a6738c5
EnhancedTarget <levente.kalman922@gmail.com>
2023-11-17 12:05:46 +0000

    w7: setting server to get all data from db

--- Week6Lab/BackEnd/server.js
+++ Week6Lab/BackEnd/server.js
@@ -63,49 +63,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-//get the book array and display it
-app.get('/api/books', (req, res) =>{
-    const books = [
-        {
-        "title": "Learn Git in a Month of Lunches",
-        "isbn": "1617292419",
-        "pageCount": 0,
-        "thumbnailUrl":"https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg",
-        "status": "MEAP",
-        "authors": ["Rick Umali"],
-        "categories": []
-        },
-        {
-        "title": "MongoDB in Action, Second Edition",
-        "isbn": "1617291609",
-        "pageCount": 0,
-        "thumbnailUrl":"https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
-        "status": "MEAP",
-        "authors": [
-        "Kyle Banker",
-        "Peter Bakkum",
-        "Tim Hawkins",
-        "Shaun Verch",
-        "Douglas Garrett"
-        ],
-        "categories": []
-        },
-        {
-        "title": "Getting MEAN with Mongo, Express, Angular, and Node",
-        "isbn": "1617292036",
-        "pageCount": 0,
-        "thumbnailUrl":"https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
-        "status": "MEAP",
-        "authors": ["Simon Holmes"],
-        "categories": []
-        }
-    ];
+//get all the records from database and set it to books variable
+app.get('/api/books', async(req, res) =>{
 
-    res.json({
-        myBooks:books,
-        "Message":"Some information",
-        "Status":"Happy"
-    });
+    //search database and bring back all records
+    let books = await bookModel.find({});
+    res.json(books);
 });
 
 //listen for requests coming in
